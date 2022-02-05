ff225980aaac79aef82d1efda2e9851c2e64394a
ALEKSANDR GOLIKOV <10749+goalrazor@users.noreply.github.yandex-team.ru>
2022-02-05 15:31:50 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function animalTalks() {
+    return catSays("Мяу") + " " +
+        dogSays() + " " + //"Гав-гав"
+        ratSays("Пи-пи-пи") + " " +
+        crowSays() + " " + //"Кар"
+        ferretSays("Кути-кути-кути") + " " +
+        monkeySays() + " " +  //"У-а-а-а"
+        frogSays("Ква-ква") + " " +
+        tigerSays() + " " + //"РРРР"
+        goatSays("Бээээ") + " " +
+        snakeSays() + " " + //"Ашассах шассах"
+        pigeonSays("Курлы-курлы");
+}
+
+console.log(animalTalks());
